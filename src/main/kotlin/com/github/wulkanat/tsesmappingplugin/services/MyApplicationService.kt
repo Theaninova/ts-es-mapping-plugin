7cb54647704de96f72d359e1765d03a7a1d185ad
GitHub Action <action@github.com>
2020-11-11 10:44:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wulkanat/tsesmappingplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wulkanat.tsesmappingplugin.services
+
+import com.github.wulkanat.tsesmappingplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
